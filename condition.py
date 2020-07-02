30c6c55cfe9195124af93051e80c4ed7745b415c
RamachandiranVijayalakshmi <66218468+RamachandiranVijayalakshmi@users.noreply.github.com>
2020-07-03 00:21:35 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,12 @@
+def cond(a,b,c):
+    if a>b and a>c:
+        print('a greater number')
+    elif b>a and b>c:
+        print('b greater number')
+    elif c>a and c>a:
+        print('c greater number')
+    else:
+        print('ture condion not in the code')  
+d,e,f=45,23,22
+cond(d,e,f)
+
